9eea09b67fd1e029cc0638394e923c9722c8086f
pranaykohad <prakohad@gmail.com>
2025-04-19 13:44:38 +0530

    added cors config

--- src/main/java/com/example/ExampleApiDemo/controller/ResumeController.java
+++ src/main/java/com/example/ExampleApiDemo/controller/ResumeController.java
@@ -3,9 +3,6 @@ package com.example.ExampleApiDemo.controller;
 import com.example.ExampleApiDemo.model.ResumeData;
 import com.example.ExampleApiDemo.service.ResumeService;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
@@ -20,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @RestController
@@ -36,14 +32,6 @@ public class ResumeController {
     this.resumeService = resumeService;
   }
 
-  // @PostMapping(value = "/parse", consumes =
-  // MediaType.MULTIPART_FORM_DATA_VALUE)
-  // public ResponseEntity<ResumeData> parseResume(@RequestParam("file")
-  // MultipartFile file) throws IOException {
-  // ResumeData resumeData = resumeService.extractResumeData(file);
-  // return ResponseEntity.ok(resumeData);
-  // }
-
   @PostMapping("parse")
   public ResponseEntity<ResumeData> extract(@RequestParam("file") MultipartFile file) throws IOException {
     ResumeData data = resumeService.extractResumeData(file);
@@ -53,9 +41,12 @@ public class ResumeController {
   @PostMapping("download-resume")
   public ResponseEntity<byte[]> downloadResume(@RequestBody ResumeData resumeData) throws IOException {
     byte[] bytes = resumeService.downloadResume(resumeData);
+    String fileName = resumeData.getHeaders().getCandidateName() + ".docx";
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-    headers.setContentDisposition(ContentDisposition.attachment().filename("resume.pdf").build());
+    headers.setContentDisposition(
+        ContentDisposition.attachment().filename(fileName).build());
+    headers.add("X-Filename", fileName);
     return new ResponseEntity<>(bytes, headers, HttpStatus.OK);
   }
 
