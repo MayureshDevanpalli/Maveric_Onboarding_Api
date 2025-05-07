90f62d16b168b399ab36f5f42667a7c8ae572ab4
pranaykohad <prakohad@gmail.com>
2025-05-07 17:45:24 +0530

    print ip with port startup

--- src/main/java/com/example/ExampleApiDemo/ExampleApiDemoApplication.java
+++ src/main/java/com/example/ExampleApiDemo/ExampleApiDemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.ExampleApiDemo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class ExampleApiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApiDemoApplication.class, args);
+	}
+
+	@PostConstruct
+	void init() {
+		try {
+			String ipAddress = InetAddress.getLocalHost().getHostAddress();
+			String port = System.getProperty("server.port", "8080");
+			log.info("application is running on http://{}:{}", ipAddress, port);
+		} catch (UnknownHostException e) {
+			log.error("Error while running application", e.getLocalizedMessage());
+		}
 	}
 
 }
